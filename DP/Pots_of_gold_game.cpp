0713f38abc085b7944afed14d2d18869f5bdeef1
Devinder Kumar Gupta <107585@incometax.net>
2021-04-26 20:03:03 +0530

    added DP solutions

--- DP/Pots_of_gold_game.cpp
+++ DP/Pots_of_gold_game.cpp
@@ -1,3 +1,26 @@
+int maxCoins(int A[], int N) 
+{
+    int dp[1001][1001];
+    for(int g = 0; g < N; g++){
+        for(int i = 0, j = g; j < N; i++, j++){
+            if(i == j){
+                dp[i][j] = A[i];
+            }
+            else if(j - i == 1){
+                dp[i][j] = max(A[i], A[j]);
+            }
+            else{
+                int val1 = A[i] + min(dp[i+2][j], dp[i+1][j-1]);
+                int val2 = A[j] + min(dp[i+1][j-1], dp[i][j-2]);
+                dp[i][j] = max(val1, val2);
+            }
+        }
+    }
+    return dp[0][N-1];
+}
+
+//OR
+
 int dp[1001][1001];
 int maxAmount(int arr[], int i, int j){
     if(j == i){
