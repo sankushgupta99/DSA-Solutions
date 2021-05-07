4afe177f111bbfde29da051114073234040160a6
Devinder Kumar Gupta <107585@incometax.net>
2021-05-08 00:29:40 +0530

    Added Tree Solutions

---
+++ Trees_and_BST/Flatten_Binary_Tree_to_LL.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    TreeNode* flattenUtil(TreeNode* root){
+        if(root == NULL)
+            return root;
+        
+        TreeNode* left_ret = flattenUtil(root->left);
+        TreeNode* right_ret = flattenUtil(root->right);
+        
+        if(root->left == NULL && root->right == NULL){
+            return root;
+        }
+        else if(root->right != NULL && root->left == NULL){
+            return right_ret;
+        }
+        else if(root->right == NULL && root->left != NULL){
+            root->right = root->left;
+            root->left = NULL;
+            return left_ret;
+        }
+        else{
+            TreeNode* temp = root->right;
+            root->right = root->left;
+            left_ret->right = temp;
+            root->left = NULL;
+            return right_ret;
+        }
+    }
+    void flatten(TreeNode* root) {
+        flattenUtil(root);
+    }
+};
