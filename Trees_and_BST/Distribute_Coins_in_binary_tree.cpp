cb1c6f72552af410ff98589bfc955147040a8213
Devinder Kumar Gupta <107585@incometax.net>
2021-05-07 09:43:14 +0530

    Added Tree Solutions

--- Trees_and_BST/Distribute_Coins_in_binary_tree.cpp
+++ Trees_and_BST/Distribute_Coins_in_binary_tree.cpp
@@ -4,6 +4,28 @@ public:
         if(root == NULL)
             return 0;
         
+        int left_cost = distributeCoinsUtil(root->left, cost);
+        int right_cost = distributeCoinsUtil(root->right, cost);
+        
+        cost += (abs(left_cost) + abs(right_cost));
+        
+        return left_cost + right_cost + root->val - 1;
+    }
+    int distributeCoins(TreeNode* root) {
+        int cost = 0;
+        distributeCoinsUtil(root, cost);
+        return cost;
+    }
+};
+
+//OR
+
+class Solution {
+public:
+    int distributeCoinsUtil(TreeNode* root, int &cost){
+        if(root == NULL)
+            return 0;
+        
         int left = distributeCoinsUtil(root->left, cost);
         int right = distributeCoinsUtil(root->right, cost);
         int curr = left + right + root->val - 1;
