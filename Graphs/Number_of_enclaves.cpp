ac12a0cc83bfe89b8687aff24e276ecb30add903
Devinder Kumar Gupta <107585@incometax.net>
2021-05-19 15:19:17 +0530

    Added graph Solutions

--- Graphs/Number_of_enclaves.cpp
+++ Graphs/Number_of_enclaves.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     void DFS(int m, int n, vector<vector<int>> &grid, int i, int j){
+        if(i < 0 || j < 0 || i == m || j == n || !grid[i][j])
+            return;
+        grid[i][j] = 0;
+        DFS(m, n, grid, i+1, j); DFS(m, n, grid, i-1, j); 
+        DFS(m, n, grid, i, j+1); DFS(m, n, grid, i, j-1);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+                if(i*j == 0 || i == m-1 || j == n-1)
+                    DFS(m, n, grid, i, j);
+        int ans = 0;
+        for(auto i : grid)
+            for(auto j : i)
+                if(j == 1) 
+                  ans++;
+            
+        return ans;
+    }
+};
+
+//OR
+
+class Solution {
+public:
+    void DFS(int m, int n, vector<vector<int>> &grid, int i, int j){
         grid[i][j] = 0;
         if(i+1 <= m-1 && grid[i+1][j])
             DFS(m, n, grid, i+1, j);
