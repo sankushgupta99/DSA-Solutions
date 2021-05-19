36d38550270b3b69e1d008e85e6fe39c231f9819
Devinder Kumar Gupta <107585@incometax.net>
2021-05-19 15:06:58 +0530

    Added graph Solutions

---
+++ Graphs/Number_of_enclaves.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void DFS(int m, int n, vector<vector<int>> &grid, int i, int j){
+        grid[i][j] = 0;
+        if(i+1 <= m-1 && grid[i+1][j])
+            DFS(m, n, grid, i+1, j);
+        
+        if(i-1 >= 0 && grid[i-1][j])
+            DFS(m, n, grid, i-1, j);
+        
+        if(j+1 <= n-1 && grid[i][j+1])
+            DFS(m, n, grid, i, j+1);
+        
+        if(j-1 >= 0 && grid[i][j-1])
+            DFS(m, n, grid, i, j-1);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int i = 0; i < m; i++)
+            if(grid[i][0] == 1)
+                DFS(m, n, grid, i, 0);
+        
+        for(int i = 1; i < n; i++)
+            if(grid[m-1][i] == 1)
+                DFS(m, n, grid, m-1, i);
+        
+        for(int i = m - 2; i >= 0; i--)
+            if(grid[i][n-1] == 1)
+                DFS(m, n, grid, i, n-1);
+        
+        for(int i = n - 2; i >= 1; i--)
+            if(grid[0][i] == 1)
+                DFS(m, n, grid, 0, i);
+        
+        int ans = 0;
+        for(auto i : grid){
+            for(auto j : i){
+                if(j == 1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
