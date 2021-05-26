d08e4b054f2e23b9b90cb27b4b16b8e0579d8408
Devinder Kumar Gupta <107585@incometax.net>
2021-05-26 21:31:41 +0530

    Added Graph Solutions

--- Graphs/Strongly_Connected_Components(Kosaraju).cpp
+++ Graphs/Strongly_Connected_Components(Kosaraju).cpp
@@ -46,3 +46,68 @@ class Solution
         return scc;
     }
 };
+
+//OR
+
+class Solution
+{
+	public:
+    void DFS(vector<int> adj[], int src, int visited[], stack<int> &s){
+        visited[src] = 1;
+        
+        for(int i : adj[src]){
+            if(!visited[i]){
+                DFS(adj, i, visited, s);
+            }
+        }
+        
+        s.push(src);
+    }
+    
+    void DFS2(vector<int> adj[], int src, int visited[], vector<int> &v){
+        visited[src] = 1;
+        v.push_back(src);
+        
+        for(int i : adj[src]){
+            if(!visited[i]){
+                DFS2(adj, i, visited, v);
+            }
+        }
+    }
+    
+    vector<vector<int>> tarjans(int V, vector<int> adj[])
+    {
+        stack<int> s;
+        int visited[V];
+        memset(visited, 0, sizeof(visited));
+        
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                DFS(adj, i, visited, s);
+            }
+        }
+        
+        vector<int> adj2[V];
+        for(int i = 0; i < V; i++){
+            for(int j : adj[i]){
+                adj2[j].push_back(i);
+            }
+        }
+        
+        memset(visited, 0, sizeof(visited));
+        vector<vector<int>> res;
+        while(!s.empty()){
+            int curr = s.top();
+            s.pop();
+            
+            if(!visited[curr]){
+                vector<int> v;
+                DFS2(adj2, curr, visited, v);
+                sort(v.begin(), v.end());
+                res.push_back(v);
+            }
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
