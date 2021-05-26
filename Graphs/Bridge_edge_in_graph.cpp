5763698689d390434d5f76616b5eb61935d64c10
Devinder Kumar Gupta <107585@incometax.net>
2021-05-26 10:04:08 +0530

    Added Graph Solutions

--- Graphs/Bridge_edge_in_graph.cpp
+++ Graphs/Bridge_edge_in_graph.cpp
@@ -1,6 +1,54 @@
 class Solution
 {
 	public:
+    int par[100001], disc[100001], low[100001];
+    
+    void DFS(vector<int> adj[], int src, int &curr, int c, int d, bool &isbridge){
+        disc[src] = curr++;
+        
+        for(int i : adj[src]){
+            if(i == par[src])
+            continue;
+            
+            par[i] = src;
+            if(disc[i] != -1){
+                low[src] = min(low[src], disc[i]);
+            }
+            else{
+                DFS(adj, i, curr, c, d, isbridge);
+                if(low[i] > disc[src] && ((src == d && i == c) || (src == c && i == d)))
+                isbridge = true;
+                low[src] = min(low[src], low[i]);
+            }
+        }
+    }
+    
+    int isBridge(int V, vector<int> adj[], int c, int d) 
+    {
+        memset(par, -1, sizeof(par));
+        memset(disc, -1, sizeof(disc));
+        
+        for(int i = 0; i < V; i++)
+        low[i] = INT_MAX;
+        
+        int curr = 1;
+        bool isbridge = false;
+        
+        for(int i = 0; i < V; i++){
+            if(disc[i] == -1){
+                DFS(adj, i, curr, c, d, isbridge);
+            }
+        }
+        
+        return isbridge;
+    }
+};
+
+//OR
+
+class Solution
+{
+	public:
 	void DFS(vector<int> adj[], int src, int visited[], int c, int d){
         visited[src] = 1;
         for(int i : adj[src]){
