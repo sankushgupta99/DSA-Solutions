64f30deaf7f93d3bfe3dc4683fe0c8b6a1f9afb8
Devinder Kumar Gupta <107585@incometax.net>
2021-05-30 15:50:23 +0530

    Added Maths Solutions

--- Mathematics/Remainder_with_7.cpp
+++ Mathematics/Remainder_with_7.cpp
@@ -2,6 +2,30 @@ class Solution{
     public:
     int remainderWith7(string n)
     {
+        int series[] = {1, 3, 2, -1, -3, -2};
+        int series_index = 0;
+        int ans = 0;
+        
+        for(int i = n.size() - 1; i >= 0; i--){
+            int digit = n[i] - '0';
+            ans = ans + (digit * series[series_index]);
+            series_index = (series_index + 1) % 6;
+            ans = ans % 7;
+        }
+        
+        if(ans < 0)
+        ans += 7;
+        
+        return ans;
+    }
+};
+
+//OR
+
+class Solution{
+    public:
+    int remainderWith7(string n)
+    {
         int powerOfTen = 1;
         int ans = 0;
         for(int i = n.size()-1; i >= 0; i--){
