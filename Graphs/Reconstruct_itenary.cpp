c861c6527804ed4daf027e85696af8cf3022d08b
Devinder Kumar Gupta <107585@incometax.net>
2021-06-04 11:38:44 +0530

    Added Graphs Solutions

---
+++ Graphs/Reconstruct_itenary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void DFS(map<string, priority_queue<string, vector<string>, greater<string>>> &adj, string src, stack<string> &s){
+        while(!adj[src].empty()){
+            string v = adj[src].top();
+            adj[src].pop();
+            DFS(adj, v, s);
+        }
+        s.push(src);
+    }
+    
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        map<string, priority_queue<string, vector<string>, greater<string>>> adj;
+        for(auto i : tickets){
+            string u = i[0];
+            string v = i[1];
+            adj[u].push(v);
+        }
+        
+        stack<string> s;
+        DFS(adj, "JFK", s);
+        
+        vector<string> itenary;
+        while(!s.empty()){
+            itenary.push_back(s.top());
+            s.pop();
+        }
+        return itenary;
+    }
+};
