ab2223f91b989d62cf8a72535db3f6ac9b7b4b31
Devinder Kumar Gupta <107585@incometax.net>
2021-06-18 11:39:51 +0530

    Added Array Solutions

--- Arrays/Number_of_Subarrays_with_bounded_maximum.cpp
+++ Arrays/Number_of_Subarrays_with_bounded_maximum.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int si = 0, ei = 0, prevc = 0, ans = 0;
+        int n = nums.size();
+        
+        while(ei < n){
+            if(nums[ei] >= left && nums[ei] <= right){
+                prevc = ei - si + 1;
+                ans += prevc;
+            }
+            else if(nums[ei] < left){
+                ans += prevc;
+            }
+            else{
+                si = ei + 1;
+                prevc = 0;
+            }
+            ei++;
+        }
+        
+        return ans;
+    }
+};
+
+//OR
+
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
         int count = 0, count2 = 0, excluded_sum = 0, res = 0;
         
         for(int& num : nums){
