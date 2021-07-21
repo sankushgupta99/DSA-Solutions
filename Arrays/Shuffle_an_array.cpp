a0d98bc2b56fb85a3e8d8198dbca4dade3e2a02d
Devinder Kumar Gupta <107585@incometax.net>
2021-07-21 11:44:19 +0530

    Added Arrays Solutions

--- Arrays/Shuffle_an_array.cpp
+++ Arrays/Shuffle_an_array.cpp
@@ -2,6 +2,44 @@
 class Solution {
 public:
     vector<int> arr;
+    vector<int> temp;
+    vector<int> res;
+    
+    Solution(vector<int>& nums) {
+        srand((unsigned)time(NULL));
+        arr = nums;
+        temp = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return arr;
+    }
+    
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        int n = arr.size();
+        res.clear();
+        
+        for(int i = n-1; i >= 0; i--){
+            int rand_ind = rand() % (i+1);
+            res.push_back(arr[rand_ind]);
+            swap(arr[rand_ind], arr[i]);
+            arr.pop_back();
+        }
+        
+        arr = temp;
+        return res;
+    }
+};
+
+//OR
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<int> arr;
     
     /** Initializes the object with the integer array nums. */
     Solution(vector<int>& nums) {
@@ -28,6 +66,38 @@ public:
         }
         
         arr = temp;
+        return res;
+    }
+};
+
+//OR
+
+#include<bits/stdc++.h>
+class Solution {
+public:
+    vector<int> arr;
+    vector<int> res;
+    
+    Solution(vector<int>& nums) {
+        srand((unsigned)time(NULL));
+        arr = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return arr;
+    }
+    
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        int n = arr.size();
+        res = arr;
+        
+        for(int i = 0; i < n; i++){
+            swap(res[i], res[rand() % n]);
+        }
+ 
         return res;
     }
 };
