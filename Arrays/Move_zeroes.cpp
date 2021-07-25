56d82ba777487db3ae805eea68f86a1376bd2243
Devinder Kumar Gupta <107585@incometax.net>
2021-07-25 12:39:23 +0530

    Added Arrays Solutions

---
+++ Arrays/Move_zeroes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int ind = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0){
+                nums[ind++] = nums[i];
+                if(i != ind-1) nums[i] = 0;
+            }
+        }
+        
+    }
+};
+
+//OR
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int ind = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] != 0)
+                nums[ind++] = nums[i];
+        }
+        
+        for(int i = ind; i < n; i++)
+            nums[i] = 0;
+        
+    }
+};
